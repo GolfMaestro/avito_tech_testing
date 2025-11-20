a16ae86a43ebf562ec68b344d5a1aa8b5a2174ee
Vlad Eryn <vlad2004e@gmail.com>
2025-11-20 14:06:58 +0300

    some fixes

--- repository/userRepository.go
+++ repository/userRepository.go
@@ -13,10 +13,4 @@ func UpdatePersonStatus(req_id string, new_status bool) {
 		fmt.Println("Something went wrong in funciton UpdatePersonNameById")
 	}
 
-	_, err2 := Pool.Exec(context.Background(),
-		"UPDATE team_members SET is_active = $1 WHERE user_id = $2", new_status, req_id)
-	if err2 != nil {
-		fmt.Println("Something went wrong in funciton UpdatePersonNameById")
-	}
-
 }
