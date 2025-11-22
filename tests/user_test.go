461f7683cd9ce45064e12c1ee6b938a2bddbf039
Vlad Eryn <vlad2004e@gmail.com>
2025-11-22 21:37:23 +0300

    added test for get user reviews if user does not exist

--- tests/user_test.go
+++ tests/user_test.go
@@ -73,3 +73,21 @@ func TestGetUserPullRequests(t *testing.T) {
 		t.Fatalf("waiting 200, get:  %d", w.Code)
 	}
 }
+
+func TestGetUserPullRequestsUserNotExist(t *testing.T) {
+
+	TestConnection(t)
+
+	repository.Pool.Exec(context.Background(), "TRUNCATE TABLE users CASCADE")
+	repository.Pool.Exec(context.Background(), "INSERT INTO users(user_id, username, team_name, is_active) VALUES ('u1', 'bot1', 't1', false);")
+	repository.Pool.Exec(context.Background(), "INSERT INTO pull_requests(pull_request_id, pull_request_name, author_id, assigned_reviewers) VALUES ('pr1', 'update_1', 'u1', '{\"u125\"}');")
+
+	req := httptest.NewRequest(http.MethodGet, "/users/getReview?user_id=u125", nil)
+	w := httptest.NewRecorder()
+
+	service.GetUserReviews(w, req)
+
+	if w.Code != http.StatusNotFound {
+		t.Fatalf("waiting 404, get:  %d", w.Code)
+	}
+}
