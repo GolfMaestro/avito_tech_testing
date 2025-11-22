341ed096ca5ebe4a5428f7df0f9686cc11101b5b
Vlad Erin <vlad2004e@gmail.com>
2025-11-22 12:02:12 +0300

    test on add new team

--- tests/team_test.go
+++ tests/team_test.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -35,6 +36,31 @@ func TestGetTeam(t *testing.T) {
 
 	if len(teamMembers) != 0 {
 		t.Fatalf("waiting 0, get: %d", len(teamMembers))
+	}
+
+}
+
+func TestAddTeam(t *testing.T) {
+
+	TestConnection(t)
+
+	repository.Pool.Exec(context.Background(), "TRUNCATE TABLE teams CASCADE")
+
+	values := strings.NewReader("{\"team_name\": \"t1\", \"members\": []}")
+	req := httptest.NewRequest(http.MethodPost, "/persons", values)
+	req.Header.Set("Content-Type", "application/json")
+
+	w := httptest.NewRecorder()
+	service.CreateNewTeam(w, req)
+
+	// if w.Code != http.StatusCreated {
+	// 	t.Fatalf("waiting 201, get:  %d", w.Code)
+	// }
+
+	var count int
+	repository.Pool.QueryRow(context.Background(), "SELECT COUNT(*) FROM teams").Scan(&count)
+	if count != 1 {
+		t.Fatal("Team not saved in db")
 	}
 
 }
